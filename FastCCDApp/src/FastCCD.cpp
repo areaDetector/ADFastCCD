4e62e5181ac6a543cd3b0771db7342fe3f15cbe4
Stuart B. Wilkins <swilkins@bnl.gov>
2015-03-17 21:07:23 -0400

    BUG: Removed WIP

--- FastCCDApp/src/FastCCD.cpp
+++ FastCCDApp/src/FastCCD.cpp
@@ -725,7 +725,7 @@ void FastCCD::dataStatsTask(void)
     setIntegerParam(FastCCDDroppedPck, (int)stats.dropped_packets);
     setIntegerParam(FastCCDBadPck, (int)stats.mallformed_packets);
     setIntegerParam(FastCCDLastFrame, stats.last_frame);
-    setDoubleParam(FastCCDDataRate, stats.datarate);
+    //setDoubleParam(FastCCDDataRate, stats.datarate);
     
     this->lock();
     callParamCallbacks();
