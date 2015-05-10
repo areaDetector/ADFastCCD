88c460dec8cac26460e0e094258d761ebfebd0e8
Stuart B. Wilkins <swilkins@bnl.gov>
2015-05-10 10:32:49 -0400

    Added String definitions and parameters for BIAS Settings

--- FastCCDApp/src/FastCCD.h
+++ FastCCDApp/src/FastCCD.h
@@ -12,79 +12,101 @@
 #include "cin.h"
 #include "ADDriver.h"
 
-#define MAX_ENUM_STRING_SIZE 26
+#define MAX_ENUM_STRING_SIZE        26
 
-#define FastCCDPollingPeriodString              "POLL_PERIOD"
+#define FastCCDPollingPeriodString  "POLL_PERIOD"
 
-#define FastCCDMux1String                       "FCCD_MUX1"
-#define FastCCDMux2String                       "FCCD_MUX2"
+#define FastCCDMux1String           "FCCD_MUX1"
+#define FastCCDMux2String           "FCCD_MUX2"
 
-#define FastCCDFirmwarePathString               "FIRMWARE_PATH"
-#define FastCCDBiasPathString                   "BIAS_PATH"
-#define FastCCDClockPathString                  "CLOCK_PATH"
-#define FastCCDFCRICPathString                  "FCRIC_PATH"
+#define FastCCDFirmwarePathString   "FIRMWARE_PATH"
+#define FastCCDBiasPathString       "BIAS_PATH"
+#define FastCCDClockPathString      "CLOCK_PATH"
+#define FastCCDFCRICPathString      "FCRIC_PATH"
 
-#define FastCCDFirmwareUploadString             "FIRMWARE_UPLOAD"
-#define FastCCDClockUploadString                "CLOCK_UPLOAD"
-#define FastCCDBiasUploadString                 "BIAS_UPLOAD"
-#define FastCCDFCRICUploadString                "FCRIC_UPLOAD"
+#define FastCCDFirmwareUploadString "FIRMWARE_UPLOAD"
+#define FastCCDClockUploadString    "CLOCK_UPLOAD"
+#define FastCCDBiasUploadString     "BIAS_UPLOAD"
+#define FastCCDFCRICUploadString    "FCRIC_UPLOAD"
 
-#define FastCCDPowerString                      "CIN_POWER"
-#define FastCCDFPPowerString                    "CIN_FP_POWER"
-#define FastCCDCameraPowerString					      "CAMERA_POWER"
+#define FastCCDPowerString          "CIN_POWER"
+#define FastCCDFPPowerString        "CIN_FP_POWER"
+#define FastCCDCameraPowerString    "CAMERA_POWER"
 
-#define FastCCDBiasString					              "BIAS"
-#define FastCCDClocksString					            "CLOCKS"
+#define FastCCDBiasString           "BIAS"
+#define FastCCDClocksString         "CLOCKS"
 
-#define FastCCDFPGAStatusString                 "FPGA_STATUS"
-#define FastCCDDCMStatusString                  "DCM_STATUS"
+#define FastCCDFPGAStatusString     "FPGA_STATUS"
+#define FastCCDDCMStatusString      "DCM_STATUS"
 
-#define FastCCDLibCinVersionString              "LIBCINVER"
-#define FastCCDBoardIDString                    "BOARD_ID"
-#define FastCCDSerialNumString                  "SERIAL_NUM"
-#define FastCCDFPGAVersionString                "FPGA_VER"
+#define FastCCDLibCinVersionString  "LIBCINVER"
+#define FastCCDBoardIDString        "BOARD_ID"
+#define FastCCDSerialNumString      "SERIAL_NUM"
+#define FastCCDFPGAVersionString    "FPGA_VER"
 
-#define FastCCDStatusHBString                   "STATUS_HB"
+#define FastCCDStatusHBString       "STATUS_HB"
 
-#define FastCCDBadPckString                     "BAD_PCK"
-#define FastCCDDroppedPckString                 "DROPPED_PCK"
-#define FastCCDLastFrameString                  "LAST_FRAME"
-#define FastCCDResetStatsString                 "RESET_STATS"
-#define FastCCDPacketBufferString               "PACKET_BUFFER"
-#define FastCCDFrameBufferString                "FRAME_BUFFER"
-#define FastCCDImageBufferString                "IMAGE_BUFFER"
+#define FastCCDBadPckString         "BAD_PCK"
+#define FastCCDDroppedPckString     "DROPPED_PCK"
+#define FastCCDLastFrameString      "LAST_FRAME"
+#define FastCCDResetStatsString     "RESET_STATS"
+#define FastCCDPacketBufferString   "PACKET_BUFFER"
+#define FastCCDFrameBufferString    "FRAME_BUFFER"
+#define FastCCDImageBufferString    "IMAGE_BUFFER"
 
-#define FastCCDOverscanString                   "OVERSCAN"
+#define FastCCDOverscanString       "OVERSCAN"
 
-#define FastCCDFclkString                       "FCLK"
+#define FastCCDFclkString           "FCLK"
 
-#define FastCCDFCRICGainString                  "FCRIC_GAIN"
+#define FastCCDFCRICGainString      "FCRIC_GAIN"
 
-#define FastCCDVBus12V0String                   "VBUS_12V0"
-#define FastCCDVMgmt3v3String                   "VMGMT_3V3"
-#define FastCCDVMgmt2v5String                   "VMGMT_2V5"
-#define FastCCDVMgmt1v2String                   "VMGMT_1V2"
-#define FastCCDVEnet1v0String                   "VENET_1V0"
-#define FastCCDVS3E3v3String                    "VS3E_3V3"
-#define FastCCDVGen3v3String                    "VGEN_3V3"
-#define FastCCDVGen2v5String                    "VGEN_2V5"
-#define FastCCDV60v9String                      "V6_0V9"
-#define FastCCDV61v0String                      "V6_1V0"
-#define FastCCDV62v5String                      "V6_2V5"
-#define FastCCDVFpString                        "VFP"
+#define FastCCDVBus12V0String       "VBUS_12V0"
+#define FastCCDVMgmt3v3String       "VMGMT_3V3"
+#define FastCCDVMgmt2v5String       "VMGMT_2V5"
+#define FastCCDVMgmt1v2String       "VMGMT_1V2"
+#define FastCCDVEnet1v0String       "VENET_1V0"
+#define FastCCDVS3E3v3String        "VS3E_3V3"
+#define FastCCDVGen3v3String        "VGEN_3V3"
+#define FastCCDVGen2v5String        "VGEN_2V5"
+#define FastCCDV60v9String          "V6_0V9"
+#define FastCCDV61v0String          "V6_1V0"
+#define FastCCDV62v5String          "V6_2V5"
+#define FastCCDVFpString            "VFP"
 
-#define FastCCDIBus12V0String                   "IBUS_12V0"
-#define FastCCDIMgmt3v3String                   "IMGMT_3V3"
-#define FastCCDIMgmt2v5String                   "IMGMT_2V5"
-#define FastCCDIMgmt1v2String                   "IMGMT_1V2"
-#define FastCCDIEnet1v0String                   "IENET_1V0"
-#define FastCCDIS3E3v3String                    "IS3E_3V3"
-#define FastCCDIGen3v3String                    "IGEN_3V3"
-#define FastCCDIGen2v5String                    "IGEN_2V5"
-#define FastCCDI60v9String                      "I6_0V9"
-#define FastCCDI61v0String                      "I6_1V0"
-#define FastCCDI62v5String                      "I6_2V5"
-#define FastCCDIFpString                        "IFP"
+#define FastCCDIBus12V0String       "IBUS_12V0"
+#define FastCCDIMgmt3v3String       "IMGMT_3V3"
+#define FastCCDIMgmt2v5String       "IMGMT_2V5"
+#define FastCCDIMgmt1v2String       "IMGMT_1V2"
+#define FastCCDIEnet1v0String       "IENET_1V0"
+#define FastCCDIS3E3v3String        "IS3E_3V3"
+#define FastCCDIGen3v3String        "IGEN_3V3"
+#define FastCCDIGen2v5String        "IGEN_2V5"
+#define FastCCDI60v9String          "I6_0V9"
+#define FastCCDI61v0String          "I6_1V0"
+#define FastCCDI62v5String          "I6_2V5"
+#define FastCCDIFpString            "IFP"
+
+#define FastCCDBiasPosHString       "BIAS_POSH"
+#define FastCCDBiasNegHString       "BIAS_NEGH"
+#define FastCCDBiasPosRGString      "BIAS_POSRG"
+#define FastCCDBiasNegRGString      "BIAS_NEGRG"
+#define FastCCDBiasPosSWString      "BIAS_POSSW"
+#define FastCCDBiasNegSWString      "BIAS_NEGSW"
+#define FastCCDBiasPosVString       "BIAS_POSV"
+#define FastCCDBiasNegVString       "BIAS_NEGV"
+#define FastCCDBiasPosTGString      "BIAS_POSTG"
+#define FastCCDBiasNegTGString      "BIAS_NEGTG"
+#define FastCCDBiasPosVFString      "BIAS_POSVF"
+#define FastCCDBiasNegVFString      "BIAS_NEGVF"
+#define FastCCDBiasNEDGEString      "BIAS_NEDGE"
+#define FastCCDBiasOTGString        "BIAS_OTG"
+#define FastCCDBiasVDDRString       "BIAS_VDDR"
+#define FastCCDBiasVDDOutString     "BIAS_VDDOUT"
+#define FastCCDBiasBufBaseString    "BIAS_BUFBASE"
+#define FastCCDBiasBufDeltaString   "BIAS_BUFDELTA"
+#define FastCCDBiasSpare1String     "BIAS_SPARE1"
+#define FastCCDBiasSpare2String     "BIAS_SPARE2"
+
 
 //C Function prototypes to tie in with EPICS
 static void FastCCDStatusTaskC(void *drvPvt);
@@ -182,6 +204,28 @@ class FastCCD : public ADDriver {
   int FastCCDPacketBuffer;
   int FastCCDFrameBuffer;
   int FastCCDImageBuffer;
+
+  // Bias Settings
+  int  FastCCDBiasPosH;       
+  int  FastCCDBiasNegH;       
+  int  FastCCDBiasPosRG;      
+  int  FastCCDBiasNegRG;      
+  int  FastCCDBiasPosSW;      
+  int  FastCCDBiasNegSW;      
+  int  FastCCDBiasPosV;       
+  int  FastCCDBiasNegV;       
+  int  FastCCDBiasPosTG;      
+  int  FastCCDBiasNegTG;      
+  int  FastCCDBiasPosVF;      
+  int  FastCCDBiasNegVF;      
+  int  FastCCDBiasNEDGE;      
+  int  FastCCDBiasOTG;        
+  int  FastCCDBiasVDDR;       
+  int  FastCCDBiasVDDOut;     
+  int  FastCCDBiasBufBase;    
+  int  FastCCDBiasBufDelta;
+  int  FastCCDBiasSpare1;     
+  int  FastCCDBiasSpare2;     
 
   // Power monitor Variables
   int FastCCDPwrBus12V;
