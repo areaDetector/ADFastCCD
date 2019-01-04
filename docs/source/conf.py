9893ca5e1d1b087f4078c024b0007276c705c04f
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-04 12:17:18 -0500

    Removed theme override

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -156,11 +156,11 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+#html_context = {
+#    'css_files': [
+#        '_static/theme_overrides.css',  # override wide tables in RTD theme
+#        ],
+#     }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
